eac05d85ab704d613b4bc3474768686ecbdf3e56
Matt Rogers <codemattr@gmail.com>
2021-04-19 20:41:21 -0500

    Prevent errors when missing fields

--- src/handler/customCommandHandler.js
+++ src/handler/customCommandHandler.js
@@ -6,20 +6,22 @@ import { isNil, isEmpty, intersection } from 'lodash';
 
 export class CustomCommandHandler extends BaseHandler {
   async check(cmd) {
-    const msg = await Message.findOne({ where: { id: cmd.dataValues.MessageId }});
-    const channel = await this.client.channels.get(msg.dataValues.channel);
-    const message = await channel.fetchMessage(msg.dataValues.id);
+    try {
+      const msg = await Message.findOne({ where: { id: cmd.dataValues.MessageId } });
+      const channel = await this.client.channels.get(msg.dataValues.channel);
+      const message = await channel.fetchMessage(msg.dataValues.id);
 
-    const isCommand = !isNil(await CustomCommand.findOne({ where: { name: cmd.dataValues.name }}));
-    const isAllowed = !isEmpty(intersection(this.options.roles, message.member.roles.map((role) => role.name))) || isEmpty(this.options.roles) || this.options.roles === undefined;
-  
-    return isCommand && isAllowed;
+      const isCommand = !isNil(await CustomCommand.findOne({ where: { name: cmd.dataValues.name } }));
+      const isAllowed = this.options && message.member && !isEmpty(intersection(this.options.roles, message.member.roles.map((role) => role.name))) || isEmpty(this.options.roles) || this.options.roles === undefined;
+
+      return isCommand && isAllowed;
+    } catch (err) { return false; }
   }
 
   async handle(cmd) {
     const message = await Message.findOne({ where: { id: cmd.dataValues.MessageId }});
     const channel = message.dataValues.channel;
-    
+
     const customCommand = await CustomCommand.findOne({ where: { name: cmd.dataValues.name }});
 
     return await this.replyToChannel(channel, customCommand.dataValues.body);
