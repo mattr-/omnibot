11a916750b481da71f5267bba3f6cc4f747a4d5a
Matt Rogers <codemattr@gmail.com>
2021-06-07 15:04:15 -0500

    Change the initialization SQL to use more common idioms
    In this case, the more common idioms are snake case column names and table names.

--- init-db.sql
+++ init-db.sql
@@ -1,68 +1,68 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE TABLE "Message" (
+CREATE TABLE "message" (
   id varchar(255) UNIQUE NOT NULL,
   author varchar(100) NOT NULL,
   body varchar(2000),
   channel varchar(100),
   guild varchar(100),
   reactions varchar(100) [],
-  "createdAt" timestamp with time zone NOT NULL,
-  "updatedAt" timestamp with time zone NOT NULL
+  "created_at" timestamp with time zone NOT NULL,
+  "updated_at" timestamp with time zone NOT NULL
 );
 
-CREATE TABLE "Command" (
+CREATE TABLE "command" (
   id uuid UNIQUE NOT NULL DEFAULT uuid_generate_v4(),
   name varchar(100),
-  "createdAt" timestamp with time zone NOT NULL,
-  "updatedAt" timestamp with time zone NOT NULL,
-  "MessageId" varchar(255)
+  "created_at" timestamp with time zone NOT NULL,
+  "updated_at" timestamp with time zone NOT NULL,
+  "message_id" varchar(255)
 );
 
-CREATE TABLE "CommandData" (
+CREATE TABLE "command_data" (
   id uuid UNIQUE NOT NULL DEFAULT uuid_generate_v4(),
   key varchar(100),
   value varchar(2000),
-  "createdAt" timestamp with time zone NOT NULL,
-  "updatedAt" timestamp with time zone NOT NULL,
-  "CommandId" uuid
+  "created_at" timestamp with time zone NOT NULL,
+  "updated_at" timestamp with time zone NOT NULL,
+  "command_id" uuid
 );
 
-CREATE TABLE "Warning" (
+CREATE TABLE "warning" (
   id uuid UNIQUE NOT NULL DEFAULT uuid_generate_v4(),
   member varchar(100) NOT NULL,
   message varchar(2000) NOT NULL,
-  "createdAt" timestamp with time zone NOT NULL,
-  "updatedAt" timestamp with time zone NOT NULL
+  "created_at" timestamp with time zone NOT NULL,
+  "updated_at" timestamp with time zone NOT NULL
 );
 
-CREATE TABLE "Reminder" (
+CREATE TABLE "reminder" (
   id uuid UNIQUE NOT NULL DEFAULT uuid_generate_v4(),
   author varchar(100) NOT NULL,
   index integer NOT NULL,
   message varchar(2000) NOT NULL,
-  "reminderDate" timestamp with time zone NOT NULL,
-  "createdAt" timestamp with time zone NOT NULL,
-  "updatedAt" timestamp with time zone NOT NULL
+  "reminder_date" timestamp with time zone NOT NULL,
+  "created_at" timestamp with time zone NOT NULL,
+  "updated_at" timestamp with time zone NOT NULL
 );
 
-ALTER TABLE ONLY "CommandData"
-  ADD CONSTRAINT "CommandData_pkey" PRIMARY KEY (id);
+ALTER TABLE ONLY "command_data"
+  ADD CONSTRAINT "command_data_pkey" PRIMARY KEY (id);
 
-ALTER TABLE ONLY "Command"
-  ADD CONSTRAINT "Command_pkey" PRIMARY KEY (id);
+ALTER TABLE ONLY "command"
+  ADD CONSTRAINT "command_pkey" PRIMARY KEY (id);
 
-ALTER TABLE ONLY "Message"
-  ADD CONSTRAINT "Message_pkey" PRIMARY KEY (id); 
+ALTER TABLE ONLY "message"
+  ADD CONSTRAINT "message_pkey" PRIMARY KEY (id);
 
-ALTER TABLE ONLY "Warning"
-  ADD CONSTRAINT "Warning_pkey" PRIMARY KEY (id);
+ALTER TABLE ONLY "warning"
+  ADD CONSTRAINT "warning_pkey" PRIMARY KEY (id);
 
-ALTER TABLE ONLY "Reminder"
-  ADD CONSTRAINT "Remindder_pkey" PRIMARY KEY (id);
+ALTER TABLE ONLY "reminder"
+  ADD CONSTRAINT "remindder_pkey" PRIMARY KEY (id);
 
-ALTER TABLE ONLY "CommandData"
-  ADD CONSTRAINT "CommandData_CommandId_fkey" FOREIGN KEY ("CommandId") REFERENCES "Command"(id) ON UPDATE CASCADE ON DELETE SET NULL;
+ALTER TABLE ONLY "command_data"
+  ADD CONSTRAINT "command_data_command_id_fkey" FOREIGN KEY ("command_id") REFERENCES "command"(id) ON UPDATE CASCADE ON DELETE SET NULL;
 
-ALTER TABLE ONLY "Command"
-    ADD CONSTRAINT "Command_MessageId_fkey" FOREIGN KEY ("MessageId") REFERENCES "Message"(id) ON UPDATE CASCADE ON DELETE SET NULL;
+ALTER TABLE ONLY "command"
+  ADD CONSTRAINT "command_message_id_fkey" FOREIGN KEY ("message_id") REFERENCES "message"(id) ON UPDATE CASCADE ON DELETE SET NULL;
